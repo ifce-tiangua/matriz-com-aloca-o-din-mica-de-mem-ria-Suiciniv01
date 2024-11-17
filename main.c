82682e19cb09012abfc097235a775a335ce5f1b6
Suiciniv01 <viniciusfranca567@gmail.com>
2024-11-17 16:50:33 -0300

    Atv "Matriz dinamica" concluida. Aluno: Marcos Vinicius Costa de França.

--- main.c
+++ main.c
@@ -2,8 +2,39 @@
 
 int main(){
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+    int linha, coluna, i, j;
+
+    scanf("%d %d", &linha, &coluna);
+
+    int** matriz = (int**) malloc(linha * sizeof(int));
+    if (matriz == NULL) {
+        return 1;
+    }
+
+    for (i = 0; i < linha; i++){
+        matriz[i] = (int*) malloc(coluna * sizeof(int));
+        if (matriz[i] == NULL) {
+            return 1;
+        }
+    }
+
+    for (i = 0; i < linha; i++){  
+        for (j = 0; j < coluna; j++){    
+            scanf("%d", &matriz[i][j]);
+        }    
+    }
+    
+    for (i = 0; i < linha; i++){ 
+        for (j = 0; j < coluna; j++){  
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < linha; i++) {
+        free(matriz[i]);
+    }
+    free(matriz);
 
     return 0;
 }
